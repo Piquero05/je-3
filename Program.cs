c464290c43a39a5cc827ef1904b4a7b29d6b5453
Amelia <amllorenteo@chabacier.es>
2022-11-30 11:20:02 +0100

    changes

--- Program.cs
+++ Program.cs
@@ -1,6 +1,13 @@
 ﻿Console.WriteLine("Introduce un numero");
-double n1 =double.Parse(Console.ReadLine()) ;
+int n1 =int.Parse(Console.ReadLine()) ;
 Console.WriteLine("Introduce un numero");
-double n2 =double.Parse(Console.ReadLine());
-double n3 = n1/n2;
-Console.WriteLine($"El coeficiente de estos numeros es {n3.ToString("F3")}");
+int n2 =int.Parse(Console.ReadLine());
+
+if(n2>n1)
+{
+    Console.WriteLine($"El mayor de los dos es {n2.ToString("F3")} y el menor {n1.ToString("F3")}");
+}
+else
+{
+    Console.WriteLine($"El mayor de los dos es {n1.ToString("F3")} y el menor { n2.ToString("F3")}");
+}
