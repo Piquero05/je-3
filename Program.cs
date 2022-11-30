72a7c0365e679533c050927a67982dabf290b9e9
alberto <apiqueroa@chabacier.es>
2022-11-30 10:56:27 +0100

    ejercicio 3

---
+++ Program.cs
@@ -0,0 +1,6 @@
+﻿Console.WriteLine("Introduce un numero");
+double n1 =double.Parse(Console.ReadLine()) ;
+Console.WriteLine("Introduce un numero");
+double n2 =double.Parse(Console.ReadLine());
+double n3 = n1/n2;
+Console.WriteLine($"El coeficiente de estos numeros es {n3.ToString("F3")}");
